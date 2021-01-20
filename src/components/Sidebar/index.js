0f3eb77fb87c2049d7231a702eb0a2486221a17d
truongnguyenlinh <ltruong28@my.bcit.ca>
2021-01-19 21:58:39 -0800

    Update Sidebar to implement smooth-scroll

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import { SidebarContainer, Icon, CloseIcon, 
-SidebarWrapper,  SidebarMenu, SidebarLink } from './SidebarElements';
+import React, { useState, useEffect } from 'react';
+import { 
+  SidebarContainer, 
+  Icon, 
+  CloseIcon, 
+  SidebarWrapper,  
+  SidebarMenu, 
+  SidebarLink 
+} from './SidebarElements';
+import resume from "../../../src/files/resume.pdf";
+import { animateScroll as scroll} from 'react-scroll';
+
 
 const Sidebar = ({ isOpen, toggle }) => {
+  const [scrollSidebar, setScrollSidebar] = useState(false);
+
+  const changeSidebar = () => {
+    if(window.scrollY >= 80) {
+      setScrollSidebar(true);
+    } else {
+      setScrollSidebar(false);
+    }
+  };
+
+    useEffect(() => {
+      window.addEventListener('scroll', changeSidebar)
+    }, []);
+
   return (
     <>
-      <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <SidebarContainer scrollSidebar={scrollSidebar} isOpen={isOpen} onClick={toggle}>
         <Icon onClick={toggle}>
           <CloseIcon/>
         </Icon>
         <SidebarWrapper>
           <SidebarMenu>
-            <SidebarLink to="about" onClick={toggle}>
+            <SidebarLink 
+            to="about" 
+            onClick={toggle}
+            smooth={true} 
+            duration={500} 
+            spy={true} 
+            exact={"true"} 
+            offset={-80}>
               About
             </SidebarLink>
-            <SidebarLink to="resume" onClick={toggle}>
-              Resume
-            </SidebarLink>
-            <SidebarLink to="projects" onClick={toggle}>
+            <SidebarLink 
+            to="project1" 
+            onClick={toggle}
+            smooth={true} 
+            duration={500} 
+            spy={true} 
+            exact={"true"} 
+            offset={-80}>
               Projects
+            </SidebarLink>
+            <SidebarLink to="route" onClick={(event) => { event.preventDefault(); window.open(resume, "_self");}}>
+              Resume
             </SidebarLink>
           </SidebarMenu>
         </SidebarWrapper>
