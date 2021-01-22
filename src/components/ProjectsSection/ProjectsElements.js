8e1c83b918933aed5006bc4f15ab8b5ff7aa924a
truongnguyenlinh <ltruong28@my.bcit.ca>
2021-01-22 02:58:37 -0800

    Update vw to fill screen size

--- src/components/ProjectsSection/ProjectsElements.js
+++ src/components/ProjectsSection/ProjectsElements.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ProjectsContainer = styled.div`
   background: #000;
-  width: 100vw;
+  width: 100%;
   min-height: 100vh;
   padding: 5rem calc((100vw - 13000px) / 2);
   color: #FFF;
